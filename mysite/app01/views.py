ec92b8e8050cc2174650599ac23db545c32059a0
iiweixiao <iiweixiao@yeah.net>
2022-05-27 00:00:30 +0800

    sql删除表格后好像有点问题，先保存再说

--- mysite/app01/views.py
+++ mysite/app01/views.py
@@ -16,3 +16,22 @@ def login(request):
             return redirect('https://sspai.com/')
         else:
             return render(request, 'login.html', {'error_msg': '用户名或密码错误'})
+
+from  app01.models import Department, UserInfo
+def orm(request):
+    # 测试ORM操作表中的数据
+    # Department.objects.create(title='销售部')
+    # Department.objects.create(title='IT部')
+    # Department.objects.create(title='运营部')
+    UserInfo.objects.create(name="武沛齐", password="123", age=19)
+    UserInfo.objects.create(name="朱虎飞", password="666", age=29)
+    UserInfo.objects.create(name="吴阳军", password="666")
+    UserInfo.objects.filter(id=3).delete()
+    # Department.objects.all().delete()
+    # UserInfo.objects.all().delete()
+
+
+
+
+
+    return HttpResponse('成功')
