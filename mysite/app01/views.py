d6850d48b0dc28c041f778c2fd601b2b111b5ea9
iiweixiao <iiweixiao@yeah.net>
2022-05-27 00:37:22 +0800

    在models.py模块中完成增删改查

--- mysite/app01/views.py
+++ mysite/app01/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
 
+from app01.models import Department, UserInfo
+
 
 def index(request):
     return HttpResponse('欢迎使用！')
+
 
 def login(request):
     # GET请求
@@ -16,3 +19,26 @@ def login(request):
             return redirect('https://sspai.com/')
         else:
             return render(request, 'login.html', {'error_msg': '用户名或密码错误'})
+
+
+def orm(request):
+    # 增
+    Department.objects.create(title="销售部")
+    Department.objects.create(title="IT部")
+    Department.objects.create(title="运营部")
+    UserInfo.objects.create(name="武沛齐", password="123", age=19)
+    UserInfo.objects.create(name="朱虎飞", password="666", age=29)
+    UserInfo.objects.create(name="吴阳军", password="666")
+
+    # 删
+    UserInfo.objects.filter(id=3).delete()
+
+    # 改
+    UserInfo.objects.filter(id=2).update(password=789)
+
+    # 查
+    data_list = UserInfo.objects.all()
+    for obj in data_list:
+        print(obj.id, obj.name, obj.password, obj.age)
+
+    return HttpResponse('成功')
