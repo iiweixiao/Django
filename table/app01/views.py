2562dd679fccb862cca9442f3dd41a58e4871c97
iiweixiao <iiweixiao@yeah.net>
2022-05-29 10:11:27 +0800

    depart_list和dpart_add

--- table/app01/views.py
+++ table/app01/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from app01 import models
 
-# Create your views here.
+
+def depart_list(request):
+    """ 部门列表 """
+    queryset = models.Department.objects.all()
+    return render(request, 'depart_list.html', {'queryset': queryset})
+
+
+def depart_add(request):
+    """ 添加部门 """
+    if request.method == 'GET':
+        return render(request, 'depart_add.html')
+    title = request.POST.get('title')
+
+    models.Department.objects.create(title=title)
+
