1c9c53a7ba6987ebb9a6679739a43d68ceffb28a
iiweixiao <iiweixiao@yeah.net>
2022-05-31 00:38:12 +0800

    table1 手机靓号的增删改

--- table1/app01/models.py
+++ table1/app01/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Department(models.Model):
     """ 部门表 """
     title = models.CharField(verbose_name='部门名称', max_length=32)
+
     def __str__(self):
         return self.title
+
 
 class UserInfo(models.Model):
     """ 员工表 """
@@ -22,3 +24,23 @@ class UserInfo(models.Model):
         (2, '女')
     )
     gender = models.SmallIntegerField(verbose_name='性别', choices=gender_choices)
+
+
+class PrettyNum(models.Model):
+    """ 靓号表 """
+    mobile = models.CharField(verbose_name='手机号', max_length=11)
+    # 想要允许为空，就加上 null=True, blank=True
+    price = models.IntegerField(verbose_name='价格')
+    level_choices = (
+        (1, '1级'),
+        (2, '2级'),
+        (3, '3级'),
+        (4, '4级'),
+    )
+    level = models.SmallIntegerField(verbose_name='级别', choices=level_choices, default=1)
+
+    status_choices = (
+        (1, '已占用'),
+        (2, '未占用'),
+    )
+    status = models.SmallIntegerField(verbose_name='状态', choices=status_choices, default=2)
--- table1/app01/views.py
+++ table1/app01/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from app01 import models
 
+from django import forms
+
 
 def depart_list(request):
     queryset = models.Department.objects.all()
@@ -34,9 +36,6 @@ def depart_edit(request, nid):
 def user_list(request):
     queryset = models.UserInfo.objects.all()
     return render(request, 'user_list.html', {'queryset': queryset})
-
-
-from django import forms
 
 
 class UserModelForm(forms.ModelForm):
@@ -96,3 +95,114 @@ def user_edit(request, nid):
 def user_delete(request, nid):
     models.UserInfo.objects.filter(id=nid).delete()
     return redirect('/user/list/')
+
+
+def pretty_list(request):
+    """ 靓号列表 """
+    # select * from 表 order by level desc;  升序asc降序dsc
+    queryset = models.PrettyNum.objects.all().order_by('-level')
+    return render(request, 'pretty_list.html', {'queryset': queryset})
+
+
+from django.core.validators import RegexValidator
+from django.core.validators import ValidationError
+
+
+class PrettyModelForm(forms.ModelForm):
+    # 验证：方式1 字段+正则方法
+    mobile = forms.CharField(
+        label='手机号',
+        validators=[RegexValidator(r'^1[3-9]\d{9}', '手机号格式错误'), ],
+    )
+
+    class Meta:
+        model = models.PrettyNum
+        # fields = ['mobile', 'price', 'status', 'level']
+        fields = '__all__'
+        # exclude = ['level']
+        # widgets = {
+        #     'name': forms.TextInput(attrs={'class': 'form-control'})
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
+
+    # 验证：方式2 钩子方法
+    # def clean_mobile(self):
+    #     txt_mobile = self.cleaned_data['mobile']
+    #     if len(txt_mobile) != 11:
+    #         # 验证不通过
+    #         raise ValidationError('格式错误')
+    #     # 验证通过，用户输入的值返回
+    #     return txt_mobile
+    # 用钩子方法判断添加的手机号是否在数据库中
+    def clean_mobile(self):
+        txt_mobile = self.cleaned_data['mobile']
+        exists = models.PrettyNum.objects.filter(mobile=txt_mobile).exists()
+        if exists:
+            raise ValidationError('手机号已存在')
+        return txt_mobile
+
+
+def pretty_add(request):
+    """ 新建靓号 """
+    if request.method == 'GET':
+        form = PrettyModelForm()
+        return render(request, 'pretty_add.html', {'form': form})
+    form = PrettyModelForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('/pretty/list/')
+    return render(request, 'pretty_add.html', {'form': form})
+
+
+class PrettyEditModelForm(forms.ModelForm):
+    # mobile = forms.CharField(disabled=True, label='手机号')
+    mobile = forms.CharField(
+        label='手机号',
+        validators=[RegexValidator(r'^1[3-9]\d{9}', '手机号格式错误'), ],
+    )
+
+    class Meta:
+        model = models.PrettyNum
+        fields = ['mobile', 'price', 'status', 'level']
+        # fields = '__all__'
+        # exclude = ['mobile']
+        # widgets = {
+        #     'name': forms.TextInput(attrs={'class': 'form-control'})
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs = {'class': 'form-control', 'placeholder': field.label}
+
+    def clean_mobile(self):
+
+        # 当前编辑的那一行的ID，instance.pk(primary key)
+
+        txt_mobile = self.cleaned_data['mobile']
+        exists = models.PrettyNum.objects.exclude(id=self.instance.pk).filter(mobile=txt_mobile).exists()
+        if exists:
+            raise ValidationError('手机号已存在')
+        return txt_mobile
+
+
+def pretty_edit(request, nid):
+    """ 编辑靓号 """
+    row_object = models.PrettyNum.objects.filter(id=nid).first()
+    if request.method == 'GET':
+        form = PrettyEditModelForm(instance=row_object)
+        return render(request, 'pretty_edit.html', {'form': form})
+    form = PrettyEditModelForm(data=request.POST, instance=row_object)
+    if form.is_valid():
+        form.save()
+        return redirect('/pretty/list/')
+    return render(request, 'pretty_edit.html', {'form': form})
+
+
+def pretty_delete(request, nid):
+    models.PrettyNum.objects.filter(id=nid).delete()
+    return redirect('/pretty/list/')
