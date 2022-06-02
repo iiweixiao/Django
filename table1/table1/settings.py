30f2acce0067ceab21ff6a0be3d013eda5a47c5e
wx <813703110@qq.com>
2022-06-02 16:56:39 +0800

    blog init

--- table1/table1/settings.py
+++ table1/table1/settings.py
@@ -84,9 +84,9 @@ WSGI_APPLICATION = 'table1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tb1',  # 数据库名字
+        'NAME': 'tb01',  # 数据库名字
         'USER': 'root',
-        'PASSWORD': '12345678',
+        'PASSWORD': '123456789',
         'HOST': '127.0.0.1',  # 那台机器安装了MySQL
         'PORT': 3306,
     }
