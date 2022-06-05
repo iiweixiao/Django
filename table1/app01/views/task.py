9c4e3a5896d9793dd9e438db7cea2f1070eb6fb9
iiweixiao <iiweixiao@yeah.net>
2022-06-06 02:01:28 +0800

    table1 ajax表单

--- table1/app01/views/task.py
+++ table1/app01/views/task.py
@@ -2,12 +2,27 @@ import json
 
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+from django import forms
 from django.views.decorators.csrf import csrf_exempt
+
+from app01 import models
+from app01.utils.bootstrap import BootStrapModelForm
+
+
+class TaskModelForm(BootStrapModelForm):
+    class Meta:
+        model = models.Task
+        fields = "__all__"
+        widgets = {
+            "detail": forms.TextInput,
+            # "detail": forms.Textarea,
+        }
 
 
 def task_list(request):
     """ 任务列表 """
-    return render(request, 'task_list.html')
+    form = TaskModelForm()
+    return render(request, 'task_list.html', {'form': form})
 
 
 @csrf_exempt
@@ -18,3 +33,18 @@ def task_ajax(request):
     return HttpResponse(json.dumps(data_dict))
 
     # return JsonResponse(data_dict)
+
+
+@csrf_exempt
+def task_add(request):
+    print(request.POST)
+
+    # 1.用户发送过来的数据进行校验(Mode1Form进行校验)
+    form = TaskModelForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        data_dict = {"status": True, "errors": form.errors}
+        return HttpResponse(json.dumps(data_dict))
+
+    data_dict = {"status": True, "error": form.errors}
+    return HttpResponse(json.dumps(data_dict))
