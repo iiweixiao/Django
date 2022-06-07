6bc411cf32f1b72b62e2b93b4ab100876547865a
iiweixiao <iiweixiao@yeah.net>
2022-06-07 23:56:49 +0800

    table1 ajax数据提交、删除、编辑、对话框逻辑

--- table1/table1/urls.py
+++ table1/table1/urls.py
@@ -51,4 +51,7 @@ urlpatterns = [
     # 订单管理
     path('order/list/', order.order_list),
     path('order/add/', order.order_add),
+    path('order/delete/', order.order_delete),
+    path('order/detail/', order.order_detail),
+    path('order/edit/', order.order_edit),
 ]
